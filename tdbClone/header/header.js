44bb04e120ecee4af1469361a7ca8269bb411a65
Viktor <viktor@ekastigen.net>
2025-09-12 22:08:21 +0200

    har nu fixat en menu-bar som slidar ner från under headern

--- tdbClone/header/header.js
+++ tdbClone/header/header.js
@@ -2,8 +2,22 @@
 
 const textBlocks = document.getElementsByClassName('text-block');
 for(let block of textBlocks){
-    block.addEventListener('click', () => {
-        block.parentElement.classList.toggle("open");
-        console.log("You clicked on a navbar! ");
+    block.addEventListener('click', (e) => {
+
+        let isOpen = block.parentElement.classList.contains('open');
+
+        let openNavs = document.querySelectorAll('.navbar-container.open');
+        openNavs.forEach(nav => nav.classList.remove('open'));
+
+        if(!isOpen) block.parentElement.classList.add("open");
+        
     })
 }
+
+document.addEventListener("click", (e) => {
+    let isInside = e.target.closest('.navbar-container');
+    if(!isInside){
+        let openNavs = document.querySelectorAll('.navbar-container.open');
+        openNavs.forEach(nav => nav.classList.remove('open'));
+    }
+})
