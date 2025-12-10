a1de8a33708b3becb24647b327f10d87f73c4b4c
Viktor <viktor@ekastigen.net>
2025-12-10 18:14:19 +0100

    fixed background imgae for menu and second slide. TODO fix header following and header removeal on upscroll

--- tdbClone/tdbClone/src/Menu.jsx
+++ tdbClone/tdbClone/src/Menu.jsx
@@ -6,6 +6,18 @@ export default function Menu(){
             <img src="./img/mainPage.jpg" className="menu-background-img" />
             <div className="menu-background-img-overlay"/>
             <h1 className="menu-img-text">Välkommen Till TDB-Mottagningen</h1>
+            <div className="second-menu-slide">
+                <div className="text-box-1">
+                    <h2>GRATTIS OCH VÄLKOMMEN TILL UPPSALA!</h2>
+                    <p>Du har valt att börja studera på TekNat-fakulteten vid Uppsala universitet, ett fantastiskt bra val som du knappast kommer att ångra. Måndagen den 25:a august börjar ditt otroliga studentäventyr.
+                        Vi på Uppsala teknolog- och naturvetarkår arrangerar en mottagning för dig och de andra cirka 1200 nya studenterna på TekNat-fakulteten. 
+                        <br/>
+                        <br/>
+                        Under mottagningen så kommer Instagram användas som informationskanal och det är därför viktigt att reccen följer den flitigt för att inte missa något! Reccen rekommenderas självklart att utforska snabblänkarna nedan så att reccen har koll på att dom behöver veta innan den stora dagen
+                    </p>
+
+                </div>
+            </div>
         </div>
     )
 }
