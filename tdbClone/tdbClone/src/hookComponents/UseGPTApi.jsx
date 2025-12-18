65321a56e94372623e5fbb816994182c08659870
Viktor Forslund <viktor@ekastigen.net>
2025-12-18 21:27:13 +0100

    added a rekkå chatbot

--- tdbClone/tdbClone/src/hookComponents/UseGPTApi.jsx
+++ tdbClone/tdbClone/src/hookComponents/UseGPTApi.jsx
@@ -3,11 +3,17 @@ import dotenv from "dotenv";
 
 export default async function UseGPTApi(input){
 
+    const styleText = await fetch("/style.txt").then(res => res.text());
     const client = new OpenAi({apiKey: import.meta.env.VITE_OPENAI_API_KEY, dangerouslyAllowBrowser: true});
 
     const response = await client.responses.create({
         model: "gpt-4o-mini",
-        input: [{
+        input: [
+            {role: "system",
+            content: `Du är en ai assistent som ska hjälpa nya studenter på uppsala universitet TDB-mottagningen. Du ska endast svara på frågor som rör uppsala universitet och TDB-mottagningen.
+            DU FÅR ENDAST svara med text som matchar denna textstil ${styleText}`
+            },
+            {
             role: "user",
             content: `${input}`
         }]
